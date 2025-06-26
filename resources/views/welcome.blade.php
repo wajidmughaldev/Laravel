0c0bacba2aa5a8d48b2bee305afbe646a1bcedb5
wajid mughal <wajidmughalwm2001@gmail.com>
2025-06-27 03:08:06 +0500

    latest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>welcome {{$name}}</h1>
+    <h1>welcome page</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit consequuntur iure recusandae doloremque suscipit ad, accusamus architecto nisi corporis impedit excepturi officia, consequatur ipsam maiores totam deserunt laborum quidem!</p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,3 @@ Route::get('/about',function(){
 Route::get('/contact',function(){
     return view('pages.contact');
 });
-
-// only returning view 
-
-Route::view('/welcome','welcome');
-
-// 3rd parameter is optional to pass data to route
-// Route::view('/welcome','welcome',['name'=>'wajid']);
