11ead11a550519add4b475ce0449dff74716b210
wajid mughal <wajidmughalwm2001@gmail.com>
2025-06-27 13:04:32 +0500

    covered name route

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,6 +6,10 @@
     <title>Document</title>
 </head>
 <body>
+      
+    <a href="{{route('/')}}">Home</a>
+    <a href="{{route('about')}}">About</a>
+    <a href="{{route('contact')}}">Contact</a>
     <h1>about page</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit consequuntur iure recusandae doloremque suscipit ad, accusamus architecto nisi corporis impedit excepturi officia, consequatur ipsam maiores totam deserunt laborum quidem!</p>
 </body>
