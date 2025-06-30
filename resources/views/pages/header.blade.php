a939324ede232dbedd45dfecf0503b11c4111722
wajid mughal <wajidmughalwm2001@gmail.com>
2025-06-30 17:14:48 +0500

    include subviews

---
+++ resources/views/pages/header.blade.php
@@ -0,0 +1,1 @@
+<h1>Header Page {{$name}}</h1>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,34 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-        {{-- @php
-        $username = "wajid";
-
-        @endphp
-
-        @if($username)
-            <h1>{{$username}}</h1>
-        @else
-            <h1>No Username Found</h1>
-        @endif --}}
-
-        {{-- for loop --}}
-
-        {{-- @for($i=1;$i<=10;$i++)
-           <h1> {{$i}}</h1>
-        @endfor --}}
-
-        {{-- foreach --}}
-    @php
-        $fruits=['mango','orange','cherry'];
-    @endphp
+    @include('pages.header',['name'=>'bilsign'])
     
-    @foreach ($fruits as $fruit)
-    {{$loop->index}}{{$fruit}}
-    @endforeach
-
-    {{-- <h1>welcome {{$username}}</h1> --}}
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit consequuntur iure recusandae doloremque suscipit ad, accusamus architecto nisi corporis impedit excepturi officia, consequatur ipsam maiores totam deserunt laborum quidem!</p>
+    <h1>home Page</h1>
+    @include('pages.footer')
+    {{-- @includeif('pages.content') --}}
+    @includewhen(10==20,'pages.content')
 </body>
 </html>
 
